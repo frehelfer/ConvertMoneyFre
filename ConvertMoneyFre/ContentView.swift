19ce9dcae40e4b32f9f0e3e21554b0a4903c78ef
Frédéric Helfer <fre.helfer@gmail.com>
2023-08-16 09:49:45 -0300

    Initial Commit

---
+++ ConvertMoneyFre/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ConvertMoneyFre
+//
+//  Created by Frédéric Helfer on 16/08/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ConvertMoneyFre/ConvertMoneyFreApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConvertMoneyFreApp.swift
+//  ConvertMoneyFre
+//
+//  Created by Frédéric Helfer on 16/08/23.
+//
+
+import SwiftUI
+
+@main
+struct ConvertMoneyFreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
